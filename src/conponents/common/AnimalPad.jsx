072f0d1d141271bec9cc16c5a9dff510334a4b84
park-sehyuk <tpgur0523@gmail.com>
2025-09-10 18:35:00 +0900

    animalSound

--- src/conponents/common/AnimalPad.jsx
+++ src/conponents/common/AnimalPad.jsx
@@ -1,4 +1,44 @@
+import { useEffect } from "react";
+import "./AnimalPad.css";
+
+const animals = [
+  { id: "Q", sound: "/sounds/animal/cat.mp3", title: "cat" },
+  { id: "W", sound: "/sounds/animal/chick.mp3", title: "chick" },
+  { id: "E", sound: "/sounds/animal/dog.mp3", title: "dog" },
+  { id: "A", sound: "/sounds/animal/duck.mp3", title: "duck" },
+  { id: "S", sound: "/sounds/animal/elephant.mp3", title: "elephant" },
+  { id: "D", sound: "/sounds/animal/monkey.mp3", title: "monkey" },
+  { id: "Z", sound: "/sounds/animal/pipit.mp3", title: "pipit" },
+  { id: "X", sound: "/sounds/animal/sheep.mp3", title: "sheep" },
+  { id: "C", sound: "/sounds/animal/tiger.mp3", title: "tiger" },
+];
 const AnimalPad = () => {
-  return <div id="AnimalPad">animal</div>;
+  const handlePlay = (sound) => {
+    const audio = new Audio(sound);
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const animal = animals.find((d) => d.id === event.key.toUpperCase());
+      if (animal) {
+        handlePlay(animal.sound);
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  return (
+    <div id="AnimalPad">
+      {animals.map((animal) => (
+        <button key={animal.id} onClick={() => handlePlay(animal.sound)}>
+          {animal.id} - {animal.title}
+        </button>
+      ))}
+    </div>
+  );
 };
 export default AnimalPad;
